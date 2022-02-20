e1aa5fd8a48e1fef3c3c9ed1b96a3035de75ddc2
wuran <wuran@dian.so>
2022-02-20 21:09:48 +0800

    feat:打开转发

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -14,6 +14,18 @@ class App extends Component {
     }
   }
 
+  onShareAppMessage (res) {
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: '聆思教育开团啦！',
+      path: 'pages/index/index'
+    }
+  }
+
+
   render() {
     return <Provider store={init({ models })}>
       {this.props.children}
--- client/src/pages/index/index.config.ts
+++ client/src/pages/index/index.config.ts
@@ -1,3 +1,5 @@
 export default {
-  navigationBarTitleText: '聆思教育'
+  navigationBarTitleText: '聆思教育',
+  enableShareAppMessage: true,
+  enableShareTimeline: true
 }
