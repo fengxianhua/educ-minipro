7591763f9046c8ed63214a4b47978c850665252f
wuran <wuran@dian.so>
2022-08-24 11:35:46 +0800

    feat: 课程选择

--- cloud/functions/download/index.js
+++ cloud/functions/download/index.js
@@ -14,12 +14,12 @@ exports.main = async (data) => {
     }
   }
   try {
-    let StuInfo = await db.collection('education').get()
+    let StuInfo = await db.collection('educationV2').get()
     //将获取到的数据对象赋值给变量，接下来需要用该对象向Excel表中添加数据
     let dataCVS = `studentInfo-${Math.floor(Math.random()*1000000000)}.xlsx`
     //声明一个Excel表，表的名字用随机数产生
     let alldata = [];
-    let row = ['姓名', '年龄', '校区', '联系电话', '报名时间', '新生老生', '是否团长', '参团人数'];
+    let row = ['姓名', '年龄', '校区', '课程', '联系电话', '报名时间', '新生老生', '是否团长', '参团人数'];
     //表格的属性，也就是表头说明对象
     alldata.push(row);  //将此行数据添加到一个向表格中存数据的数组中
     //接下来是通过循环将数据存到向表格中存数据的数组中
@@ -28,6 +28,7 @@ exports.main = async (data) => {
       arr.push(StuInfo.data[key].userName);
       arr.push(StuInfo.data[key].age);
       arr.push(StuInfo.data[key].school);
+      arr.push(StuInfo.data[key].className);
       arr.push(StuInfo.data[key].phone);
       arr.push(StuInfo.data[key].time);
       arr.push(StuInfo.data[key].isNew ? '新生' : '老生');
